b46a4113a4d03962e2025c2b6715564b4e431782
Nasrullah774 <57481883+Nasrullah774@users.noreply.github.com>
2021-05-03 05:59:59 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function clr(){
+    document.getElementById('scr').value=" "
+
+}
+function number( a){
+    document.getElementById('scr').value+=a
+}
+function eva(){
+ var z=document.getElementById('scr')
+    z.value=eval(z.value)
+}
